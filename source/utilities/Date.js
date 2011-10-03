2bcabc53e2eb3f82acb094db4a025fa192f7eff9
Robert Sargant <robert@sargant.com>
2011-10-03 19:00:33 +0100

    Might have been giving incorrect dates before

--- source/utilities/Date.js
+++ source/utilities/Date.js
@@ -1,10 +1,14 @@
 reddOS_Date = {
     
     timeSince: function(utc) {
-        var seconds = Math.round(Date.now()/1000) - utc;
+        var seconds = this.now() - utc;
         if(seconds > 86400) { return Math.floor(seconds/86400) +  " days";}
         if(seconds > 3600)  { return Math.floor(seconds/3600) +  " hours";}
         if(seconds > 60)  { return Math.floor(seconds/60) +  " minutes";}
         return seconds + " seconds";
     },
+    
+    now: function () {
+        return Math.round(new Date().getTime()/1000.0);
+    }
 };
